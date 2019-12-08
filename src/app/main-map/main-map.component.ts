174dd86b46e63e0b1915b1c3bf7e9978c090c4fb
Your Name <you@example.com>
2019-12-08 19:22:40 +0100

    three

---
+++ src/app/main-map/main-map.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-map',
+  templateUrl: './main-map.component.html',
+  styleUrls: ['./main-map.component.css']
+})
+export class MainMapComponent implements OnInit {
+
+  zurl="assets/img/Mapa_demo_2_11x60.png";
+  pos="50% 50%";
+  background_position_x = '50%';
+  background_position_y = '50%';
+
+  eo : number = 0;
+  ew : number = 0;
+  eoy : number = 0;
+  eh : number = 0;
+
+   backgroundUrl="assets/img/Agonia.Grid.png"
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+ public myfunction(event : any){
+   let element = event.target;
+   this.eo = - event.offsetX+20;
+   this.ew = element.width
+   this.eoy = - event.offsetY+20;
+   this.eh = element.height
+   this.background_position_x = ''+this.eo *10 +'px';
+   this.background_position_y = ''+this.eoy* 10 +'px';
+   
+ // alert(" eoy:"+eoy+" eh:"+eh);
+  }
+
+}
